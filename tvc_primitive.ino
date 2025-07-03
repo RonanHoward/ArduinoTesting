a0824d40389113475d19c282821345f8afdcfdb6
Ronan Howard <47797358+RonanHoward@users.noreply.github.com>
2025-07-03 12:30:19 -0400

    Update tvc_primitive.ino

--- tvc_primitive.ino
+++ tvc_primitive.ino
@@ -1,7 +1,7 @@
 /******************************************************************************
 
-Proportional Correction Test V0R0
-MAY 2025 (c) Concept Propulsion.  All Rights Reserved.
+Proportional Correction Test V0R1
+JULY 2025 (c) Concept Propulsion.  All Rights Reserved.
 R. HOWARD
 
 Portions of avr-libc are Copyright (c) 1999-2016  All Rights Reserved
@@ -30,8 +30,8 @@ SCK~D13 |1        30| D12~CIPO                     |1        30|
      A3 |7        24| D6~                          |7        24|
  SDA A4 |8        23| D5~                          |8        23|
  SCL A5 |9        22| D4~                          |9        22|
-     A6 |10       21| D3~                          |10       21| Y Servo
-     A7 |11       20| D2~                          |11       20| X Servo
+     A6 |10       21| D3~                          |10       21|
+     A7 |11       20| D2~                          |11       20|
     +5v |12       19| GND                      +5V |12       19| GND
   RESET |13       18| RESET                  RESET |13       18| RESET
     GND |14       17| D1 RX                    GND |14       17|
@@ -52,21 +52,26 @@ SCK~D13 |1        30| D12~CIPO                     |1        30|
 #include <Servo.h>
 
 Servo Xaxis_servo;
-// Servo Yaxis_servo;
+Servo Yaxis_servo;
 
-// pin definitions
+// EDITORIAL VARIABLES (TUNE THESE)
 
+const float SERVO_ANGLE_LIMIT = 1.0;        // use 1.0 for full range, 0.0 for no range. Affects servo range according to this graph https://www.desmos.com/calculator/pz3dhdu8sx
+const float PROPORTIONALITY_CONSTANT = 1.0; // tvc angle multiplied by this float each iteration. range 0.0 to 1.0 recommended
 
 // variable declarations
+
+const long MAP_RADIUS = (long) floor( 90 * SERVO_ANGLE_LIMIT );
+const long LOWER_MAP_BOUND = 90 - MAP_RADIUS;
+const long UPPER_MAP_BOUND = 90 + MAP_RADIUS;
 
 float x, y, z;
 int degreesX = 0;
 int degreesY = 0;
 
 long Xaxis_command;
-// long Yaxis_command;
+long Yaxis_command;
 
-// these variables are modified by interrupt service
 
 /********************************** SETUP *************************************/
 
@@ -75,11 +80,16 @@ void setup() {
   while (!Serial);
   Serial.println("Started");
 
-  Xaxis_servo.attach(D2);
-  // Yaxis_servo.attach(D3);
+  // use D12 and D11 for servo A and B, use D4 and D5 for servo Q1 and Q2
+  Xaxis_servo.attach(D4);
+  Yaxis_servo.attach(D5);
 
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  if (SERVO_ANGLE_LIMIT < 0.0 || SERVO_ANGLE_LIMIT > 1.0) {
+    Serial.println("SERVO_ANGLE_LIMIT must be between 0.0 and 1");
     while (1);
   }
 
@@ -96,20 +106,23 @@ void loop() {
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
 
-    // only considering xz-plane
-
-    double angleX = atan( x/z ); // bounded by [-PI/2, PI/2] which is roughly [-1.570796, 1.570796]
+    // xz-plane
+    double angleX = atan( x/z ) * PROPORTIONALITY_CONSTANT; // bounded by [-PI/2, PI/2] which is roughly [-1.570796, 1.570796]
     long intAngleX = (long) floor(angleX * 100); // new bounds: [-157, 157]
+    // yz-plane
+    double angleY = atan( y/z ) * PROPORTIONALITY_CONSTANT;
+    long intAngleY = (long) floor(angleY * 100);
 
 
     // create servo commands
 
-    Xaxis_command = map (intAngleX, -157, 157, 0, 180);
-    // yaxis_command = map ();
+    Xaxis_command = map (intAngleX, -157, 157, LOWER_MAP_BOUND, UPPER_MAP_BOUND);
+    Yaxis_command = map (intAngleY, -157, 157, LOWER_MAP_BOUND, UPPER_MAP_BOUND);
 
     // write servo commands
 
     Xaxis_servo.write(Xaxis_command);
+    Yaxis_servo.write(Yaxis_command);
     
 
     // print acceleration vector
@@ -118,11 +131,9 @@ void loop() {
     Serial.print(y);
     Serial.print('\t');
     Serial.println(z);
-    Serial.println(intAngleX);
-    Serial.println(Xaxis_command);
   }
 
-  delay(100);
+  delay(50);
 
 }
 
