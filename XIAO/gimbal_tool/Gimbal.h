678c1dff2aa8750f5cab72fc0ca8d7a5e786d5c1
Geoffrey Howard <ghoward.ee@gmail.com>
2026-08-06 17:57:59 -0400

    Updated tool

---
+++ XIAO/gimbal_tool/Gimbal.h
@@ -0,0 +1,19 @@
+#ifndef Gimbal_h
+#define Gimbal_h
+
+#include <Servo.h>
+#include <Arduino.h>
+
+class Gimbal {
+  public:
+    Gimbal();
+
+    Servo servo_roll;
+    Servo servo_pitch;
+
+    void attach();
+    void write(float vector_roll, float vector_pitch);
+    void center();
+};
+
+#endif
